9305bb0c263cf5f3c7e5099ab28a54f444c71432
Vahdetyldz <vahdet11766@gmail.com>
2025-02-25 17:01:11 +0300

    update link

--- Foody.PresentationLayer/Models/ViewComponents/DefaultViewComponents/_DeafultFooterSectionComponentPartial.cs
+++ Foody.PresentationLayer/Models/ViewComponents/DefaultViewComponents/_DeafultFooterSectionComponentPartial.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Foody.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Foody.PresentationLayer.Models.ViewComponents.DefaultViewComponents
 {
     public class _DeafultFooterSectionComponentPartial : ViewComponent
     {
+        private readonly IAdressService _adressService;
+
+        public _DeafultFooterSectionComponentPartial(IAdressService adressService)
+        {
+            _adressService = adressService;
+        }
+
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = _adressService.TGetAll();
+            return View(values);
         }
     }
 }
