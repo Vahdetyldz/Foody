f5d3596902de595c1010288d5ceb859367bcf0e9
Vahdetyldz <vahdet11766@gmail.com>
2025-02-25 16:13:16 +0300

    son video

--- Foody.PresentationLayer/Models/ViewComponents/DefaultViewComponents/_DeafultTestimonialSectionComponentPartial.cs
+++ Foody.PresentationLayer/Models/ViewComponents/DefaultViewComponents/_DeafultTestimonialSectionComponentPartial.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Foody.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Foody.PresentationLayer.Models.ViewComponents.DefaultViewComponents
 {
     public class _DeafultTestimonialSectionComponentPartial : ViewComponent
     {
+        private readonly IReviewService _reviewService;
+
+        public _DeafultTestimonialSectionComponentPartial(IReviewService reviewService)
+        {
+            _reviewService = reviewService;
+        }
+
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = _reviewService.TGetAll();
+            return View(values);
         }
     }
 }
